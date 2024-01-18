af04e4ba7ebe9ffbf8ea67662dddfee4899be0bb
Naveen Kumar <47122889+Naveen310894@users.noreply.github.com>
2024-01-18 01:46:47 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ kubectl version --client
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt-get install unzip -y
 unzip awscliv2.zip
-sudo ./aws/install
+sudo ./aws/install 
 
 echo "Installation completed successfully."
 
